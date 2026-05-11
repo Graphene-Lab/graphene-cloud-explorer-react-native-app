d1fade432a0819b1a0c55d9b9fe46424f66b7ff8
Ramazan <ramazansuleymanli@gmail.com>
2026-05-11 15:55:01 +0400

    refactor: remove duplicate utilities from parser.js
    parser.js was copy-pasting getFileType, parseDateTime, normalizeRelativePath,
    and buildFullPath instead of importing them. Replace all four local definitions
    with a single import from essential-functions, following the DRY principle.
    Also adds an explicit IsDirectory guard in parseSingle so folders can never
    fall through to the 'other' type.

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,36 +1,13 @@
 import { fileTypes } from "../constants";
 
-const getFileType = (file) => {
-    file = file?.split('.').reverse()[0].toLowerCase();
-    return fileTypes[file] ? fileTypes[file] : 'other';
-};
-
-const parseDateTime = (dateTime) => {
-    let date = new Date(dateTime);
-    // return date.toLocaleDateString() + " " + date.toLocaleTimeString();
-    return date.toLocaleDateString();
-};
-
-const normalizeRelativePath = (value) => {
-    if (!value) return '';
-    return value.replace(/\\/g, '/').replace(/^\/+|\/+$/g, '');
-};
-
-const buildFullPath = (name, loc) => {
-    const cleanName = normalizeRelativePath(name);
-    const cleanLoc = normalizeRelativePath(loc);
-    if (!cleanLoc) return cleanName;
-    if (cleanName === cleanLoc || cleanName.startsWith(cleanLoc + '/')) return cleanName;
-    if (!cleanName) return cleanLoc;
-    return `${cleanLoc}/${cleanName}`;
-};
+import { getFileType, parseDateTime, buildFullPath, normalizeRelativePath } from "./essential-functions";
 
 export const parseSingle = (obj, path) => {
     const name = obj?.Name?.replace('loudBoxNuget/Cloud0/', '');
     const fullPath = buildFullPath(name, path);
     return {
         title: name,
-        type: getFileType(obj?.Name),
+        type: obj?.IsDirectory ? 'folder' : getFileType(obj?.Name),
         name: name?.split('/').reverse()[0],
         source: 'data:image/png;base64,' + obj?.Thumbnail,
         Thumbnail: obj?.Thumbnail,
